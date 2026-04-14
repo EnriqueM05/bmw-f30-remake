f29bd30d2e394444ec61ac945fb1993947636cfe
EnriqueM05 <pokequique0527@gmail.com>
2026-04-14 07:19:10 -0600

    last changes

--- carousel.js
+++ carousel.js
@@ -1,14 +1,75 @@
-document.documentElement.classList.add("js");
+const slider = document.querySelector(".slider");
+const leftArrow = document.createElement("button");
+const rightArrow = document.createElement("button");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+leftArrow.classList.add("arrow", "left");
+rightArrow.classList.add("arrow", "right");
+
+leftArrow.textContent = "<";
+rightArrow.textContent = ">";
+
+slider.parentElement.appendChild(leftArrow);
+slider.parentElement.appendChild(rightArrow);
+
+let currentIndex = 0;
+const slides = slider.children;
+const navDots = document.querySelectorAll(".slider-nav div");
+const divs = document.querySelectorAll(".slider img");
+
+const descriptions = [
+  "Backview",
+  "Sideview",
+  "Front light",
+  "Backlight",
+  "Interior",
+];
+
+const descriptionElement = document.querySelector(".designBottom");
+
+const updateSliderPosition = () => {
+  slider.scrollTo({
+    left: slides[currentIndex].offsetLeft,
+    behavior: "smooth",
+  });
+  updateActiveDot();
+  updateActiveDiv();
+  updateDescription();
+};
+
+const updateActiveDot = () => {
+  navDots.forEach((dot, index) => {
+    if (index === currentIndex) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
     }
   });
+};
+
+const updateActiveDiv = () => {
+  divs.forEach((div, index) => {
+    if (index === currentIndex) {
+      div.classList.add("active");
+    } else {
+      div.classList.remove("active");
+    }
+  });
+};
+
+const updateDescription = () => {
+  descriptionElement.textContent = descriptions[currentIndex];
+};
+
+leftArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateSliderPosition();
 });
 
-document.querySelectorAll(".animation").forEach((el) => {
-  observer.observe(el);
+rightArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateSliderPosition();
 });
 
+updateActiveDot();
+updateActiveDiv();
+updateDescription();
